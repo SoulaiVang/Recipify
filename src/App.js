335f1157cbca575b1beaddba33e63ddae1101424
Nick Kent <nkent@macalester.edu>
2024-03-18 21:13:44 -0500

    saving(progress was made... i think)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 function App() {
   // Update recipe list
   const [ingredientsList, setIngredientsList] = useState([]);
-  const currentIngredients = ingredientsList.join(', ');
+  // const currentIngredients = ingredientsList.join(', ');
 
   const addIngredient = async () => {
     const ingredientInput = document.querySelector('.add-ingredients-input');
@@ -67,45 +67,49 @@ function App() {
           Welcome to the Recipe Finder!
         </h1>
       </div>
-      <div className="App-content">
-        <p>
-          Input ingredients here: 
+      <div className="add-ingredients">
+        <input
+          type="text"
+          placeholder="Input ingredients here..."
+          className="add-ingredients-input"
+        />
+        <p className="add-ingredients-instructions">
+          Please add one ingredient at a time
         </p>
-        <div className="add-ingredients">
-          <input
-            type="text"
-            placeholder="Type ingredients here"
-            className="add-ingredients-input"
-          />
-          <button className="search-button" onClick={searchAPI}>
-            Search
-          </button>
-          <p className="add-ingredients-instructions">
-            Please add one ingredient at a time
-          </p>
-          <div className="ingredient-buttons">
-            <button className="add-ingredient-button" onClick={addIngredient}>
-              Add Ingredient
-            </button>
-            <button className="clear-ingredients-button" onClick={resetIngredients}>
-              Clear Ingredients
-            </button>
-          </div>
-        </div>
-        <div className="show-ingredients">
-          <p>
-            Your current ingredients are: {currentIngredients}
-          </p>
-        </div>
-        <p className='display-label'>
-            Your recipe will be displayed below
-        </p>
-        <p className='recipe'>
-          Top Recipe: {currentRecipe}
-        </p>
-        <img className="recipePicture" src={currentRecipePicture}>
-        </img>
+        <button className="add-ingredient-button" onClick={addIngredient}>
+            Add Ingredient
+        </button>
       </div>
+
+      {/* Right Panel for ingredients */}
+      <div className="right-panel">
+        <p className="current-ingredients-label">
+          Current Ingredients:
+        </p>
+        <p className="ingredient-list">
+          {ingredientsList.map((ingredient, index) => (
+            <span key={index}>
+              {ingredient}
+              <br />
+            </span>
+          ))}
+        </p>
+        <button className="clear-ingredients-button" onClick={resetIngredients}>
+            Clear Ingredients
+        </button>
+        <button className="search-button" onClick={searchAPI}>
+          Search
+        </button>
+      </div>
+
+      <p className='display-label'>
+            Your recipe will be displayed below
+      </p>
+      <p className='recipe'>
+        Top Recipe: {currentRecipe}
+      </p>
+      <img className="recipePicture" src={currentRecipePicture}>
+      </img>
     </div>
   );
 }

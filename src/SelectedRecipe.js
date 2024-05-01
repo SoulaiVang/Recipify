f0ce817727a68e72e07824b1975f9eebfc925a9f
Nick Kent <nkent@macalester.edu>
2024-05-01 14:27:14 -0500

    Save

--- src/SelectedRecipe.js
+++ src/SelectedRecipe.js
@@ -20,7 +20,6 @@ const SelectedRecipe = () =>{
                 const apiKey = '3305701f558f4c9fb84207d5f712276f';
                 const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${recipe.title}&addRecipeInformation=true&addRecipeInstructions=true&addRecipeNutrition=true`);
                 setadv_information(response.data.results[0] || {});
-                // console.log(adv_information[0].steps)
             } catch(e) {
                 console.log("Error fetching recipes:", e);
             }
@@ -38,20 +37,21 @@ const SelectedRecipe = () =>{
             </h1>
             
             {/* Advanced info(time, servings, nutritional facts) */ }
+            
             <div className='time'>
                 <img src='./assets/clock.png' alt='clock photo' className='item'></img>
-                <p className='item'>Preparation time: {adv_information.readyInMinutes} Minutes</p>
+                <p className='item'>Preparation time(Minutes): {adv_information.readyInMinutes || "NOT AVAILABLE"} </p>
             </div>
             <div className='servings'>
                 <img src='./assets/servings.png' alt='clock photo' className='item2'></img>
-                <p className='item'>Number of servings: {adv_information.servings}</p>
+                <p className='item'>Number of servings: {adv_information.servings || "NOT AVAILABLE"}</p>
             </div>
             
             <div className='list'>
                 <h3 className='list-label'>Calorie breakdown: </h3>
-                <p>{adv_information.nutrition?.caloricBreakdown?.percentProtein} % Protein</p>
-                <p>{adv_information.nutrition?.caloricBreakdown?.percentFat} % Fat</p>
-                <p>{adv_information.nutrition?.caloricBreakdown?.percentCarbs} % Carbohydrates</p>
+                <p>Protein percentage: {adv_information.nutrition?.caloricBreakdown?.percentProtein || "NOT AVAILABLE"}</p>
+                <p>Fat percentage: {adv_information.nutrition?.caloricBreakdown?.percentFat || "NOT AVAILABLE"}</p>
+                <p>Carbohydrates percentage:{adv_information.nutrition?.caloricBreakdown?.percentCarbs || "NOT AVAILABLE"}</p>
             </div>
             
             {/* Ingredients */}
@@ -76,10 +76,16 @@ const SelectedRecipe = () =>{
                 <h3 className='list-label'>
                     Instructions:
                 </h3>
-                {adv_information.analyzedInstructions[0].steps.map((step_object) => {
-                    console.log("about to map elem:", step_object);
-                    return(<p>{step_object.number} . {step_object.step}</p>);
-                })}
+                {
+                    Object.keys(adv_information).length === 0 ? (
+                        <p>NO INSTRUCTIONS AVAILABLE</p>
+                    ) : (
+                        adv_information.analyzedInstructions[0].steps.map((step_object) => {
+                            console.log("about to map elem:", step_object);
+                            return(<p>{step_object.number}. {step_object.step}</p>);
+                        })
+                    )
+                }
             </div>
 
             <a href={adv_information.sourceUrl} className='recipe-url' target="_blank">Visit Recipe Page</a>
